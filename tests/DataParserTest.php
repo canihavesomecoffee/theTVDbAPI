95ca226e24cb05dc330b7cab31d9612f4d84c9a4
canihavesomecoffee <git@canihavesome.coffee>
2021-09-19 00:14:18 +0200

    Fix test

--- tests/DataParserTest.php
+++ tests/DataParserTest.php
@@ -35,7 +35,29 @@ class DataParserTest extends TestCase
     public function testParseBasicEpisodeData()
     {
         $json = [
-            json_decode('{"absoluteNumber": null, "airedEpisodeNumber": 0, "airedSeason": 0, "airedSeasonID": 0, "dvdEpisodeNumber": null, "dvdSeason": null, "episodeName": "string",  "firstAired": "2018-04-10", "id": 0, "language": { "episodeName": "en", "overview": "en" }, "lastUpdated": 1523605586, "overview": "string"}', true)
+            json_decode('{
+        "id": 7421162,
+        "seriesId": 280258,
+        "name": "Thibault Christiaensen",
+        "aired": "2019-10-29",
+        "runtime": 45,
+        "nameTranslations": [
+          "eng",
+          "nld"
+        ],
+        "overview": null,
+        "overviewTranslations": [
+          "nld"
+        ],
+        "image": "https://artworks.thetvdb.com/banners/series/280258/episodes/62076048.jpg",
+        "imageType": 11,
+        "isMovie": 0,
+        "seasons": null,
+        "number": 17,
+        "seasonNumber": 13,
+        "lastUpdated": "2020-02-08 21:20:50",
+        "finaleType": null
+      }', true)
         ];
         $return = DataParser::parseDataArray($json, EpisodeBaseRecord::class);
         static::assertContainsOnlyInstancesOf(EpisodeBaseRecord::class, $return);
@@ -43,10 +65,33 @@ class DataParserTest extends TestCase
 
     public function testParseBasicSeriesData()
     {
+        $episodeJSON = '{
+        "id": 7421162,
+        "seriesId": 280258,
+        "name": "Thibault Christiaensen",
+        "aired": "2019-10-29",
+        "runtime": 45,
+        "nameTranslations": [
+          "eng",
+          "nld"
+        ],
+        "overview": null,
+        "overviewTranslations": [
+          "nld"
+        ],
+        "image": "https://artworks.thetvdb.com/banners/series/280258/episodes/62076048.jpg",
+        "imageType": 11,
+        "isMovie": 0,
+        "seasons": null,
+        "number": 17,
+        "seasonNumber": 13,
+        "lastUpdated": "2020-02-08 21:20:50",
+        "finaleType": null
+      }';
         $json = [
-            json_decode('{"added": "string", "airsDayOfWeek": "string", "airsTime": "string", "aliases": [ "string" ], "banner": "string", "firstAired": "string", "genre": [ "string" ], "id": 0, "imdbId": "string", "lastUpdated": 0, "network": "string", "networkId": "string", "overview": "string", "rating": "string", "runtime": "string", "seriesId": 0, "seriesName": "string", "siteRating": 0, "siteRatingCount": 0, "status": "string", "zap2itId": "string"}', true),
-            json_decode('{"added": "string", "airsDayOfWeek": "string", "airsTime": "string", "aliases": [ "string" ], "banner": "string", "firstAired": "string", "genre": [ "string" ], "id": 0, "imdbId": "string", "lastUpdated": 0, "network": "string", "networkId": "string", "overview": "string", "rating": "string", "runtime": "string", "seriesId": 0, "seriesName": "string", "siteRating": 0, "siteRatingCount": 0, "status": "string", "zap2itId": "string"}', true),
-            json_decode('{"added": "string", "airsDayOfWeek": "string", "airsTime": "string", "aliases": [ "string" ], "banner": "string", "firstAired": "string", "genre": [], "id": 0, "imdbId": "string", "lastUpdated": 0, "network": "string", "networkId": "string", "overview": null, "rating": "string", "runtime": "string", "seriesId": 0, "seriesName": null, "siteRating": 0, "siteRatingCount": 0, "status": "string", "zap2itId": "string"}', true)
+            json_decode($episodeJSON, true),
+            json_decode($episodeJSON, true),
+            json_decode($episodeJSON, true)
         ];
         $return = DataParser::parseDataArray($json, SeriesBaseRecord::class);
         static::assertContainsOnlyInstancesOf(SeriesBaseRecord::class, $return);
